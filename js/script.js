795a1d8bd7c7a901fcae06080a203d43825d1ba8
Alejandro Alom <alejandroalom@gmail.com>
2025-03-22 09:04:39 +0000

    Peliculas Clasificadas y CSS retocado

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 import peliculas from './peliculas.js'
 
+const generoAccion = document.getElementById("genero-28")
+const generoThriller = document.getElementById("genero-53")
+const generoAventura = document.getElementById("genero-12")
+
+const peliculasFiltradasAccion = peliculas.filter(pelicula => pelicula.genre_ids.includes(28))
+const peliculasFiltradasThriller = peliculas.filter(pelicula => pelicula.genre_ids.includes(53))
+const peliculasFiltradasAventura = peliculas.filter(pelicula => pelicula.genre_ids.includes(12))
+
+
+peliculasFiltradasAccion.forEach(pelicula => generoAccion.innerHTML += `
+  <div class="pelicula">
+  <img src=https://image.tmdb.org/t/p/w200${pelicula.poster_path} />
+  <h2>${pelicula.title}</h2>
+  </div>
+  `
+)
+
+peliculasFiltradasThriller.forEach(pelicula => generoThriller.innerHTML += `
+  <div class="pelicula">
+  <img src=https://image.tmdb.org/t/p/w200${pelicula.poster_path} />
+  <h2>${pelicula.title}</h2>
+  </div>
+  `)
+
+  peliculasFiltradasAventura.forEach(pelicula => generoAventura.innerHTML += `
+  <div class="pelicula">
+  <img src=https://image.tmdb.org/t/p/w200${pelicula.poster_path} />
+  <h2>${pelicula.title}</h2>
+  </div>
+  `)
